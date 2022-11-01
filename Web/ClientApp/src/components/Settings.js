dc80caafdbe051f264988d729127a0d016113c97
Felix <fxsth@pm.me>
2022-11-01 16:01:31 +0100

    setting improvements

--- Web/ClientApp/src/components/Settings.js
+++ Web/ClientApp/src/components/Settings.js
@@ -1,5 +1,4 @@
 import React from "react";
-import DownloadButton from "./DownloadButton";
 
 export class Settings extends React.Component {
     constructor(props) {
@@ -24,11 +23,6 @@ export class Settings extends React.Component {
         const index = changedSettings.findIndex(object => {
             return object.key === name;
         });
-        console.log("Handle Change name: " + name)
-        console.log("Handle Change target: " + target)
-        console.log("Handle Change value: " + value)
-        console.log("Old value: " + changedSettings[index].value)
-        console.log("New value: " + value)
         this.setState(state => {
             state.settings[index].value = value
         });
@@ -40,21 +34,25 @@ export class Settings extends React.Component {
         for (let i = 0; i < settings.length; i++) {
             settings[i].value = event.target[i].value;
         }
-        settings.map(setting =>console.log(setting.key + " : " + setting.value));
+        settings.map(setting => console.log(setting.key + " : " + setting.value));
         this.updateSettings(settings);
     }
 
     render() {
         return (
-            <div>
-                <form onSubmit={this.handleSubmit}>
+            <div style={{width: "50%"}}>
+                <form onSubmit={this.handleSubmit} style={{width: "100%"}}>
                     {this.state.settings.map(setting =>
-                        <label>
-                            {setting.name}:
-                            <input type="text" defaultValue={setting.value} onChange={this.handleChange}/>
-                        </label>
-                    )}
-                    <input type="submit" value="Submit"/>
+                        <div style={{width: "100%", marginBottom: 20}}>
+                            <h6>{setting.name}:</h6>
+                            <label style={{width: "100%"}}>
+                                <input style={{width: "100%"}} type="text" defaultValue={setting.value}
+                                       onChange={this.handleChange}/>
+                            </label>
+                        </div>
+                    )} 
+                    <input type="reset" value="Cancel"/>
+                    <input style={{float: "right"}} type="submit" value="Save"/>
                 </form>
             </div>
         );
@@ -76,8 +74,6 @@ export class Settings extends React.Component {
         }).then(response => {
             if (response.status >= 200 && response.status < 300) {
                 return response;
-                console.log(response);
-                window.location.reload();
             } else {
                 console.log('Something happened wrong');
             }
